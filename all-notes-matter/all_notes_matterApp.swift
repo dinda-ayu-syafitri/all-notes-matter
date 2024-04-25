4635baef462e761d305231bb8368e940d54d4a98
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-04-25 15:40:49 +0700

    Initial Commit

---
+++ all-notes-matter/all_notes_matterApp.swift
@@ -0,0 +1,17 @@
+//
+//  all_notes_matterApp.swift
+//  all-notes-matter
+//
+//  Created by Dinda Ayu Syafitri on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct all_notes_matterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
