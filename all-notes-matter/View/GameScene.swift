7fc5403ed6661f711966bd9deec2f65c8b62ee1c
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-04-27 15:49:00 +0700

    Add: Create Sprite Node Function

--- all-notes-matter/View/GameScene.swift
+++ all-notes-matter/View/GameScene.swift
@@ -32,17 +32,30 @@ class GameScene:SKScene {
     var SaxTrumpetAudio: SKAudioNode!
     var pianoHarmonicaAudio: SKAudioNode!
 
+    func createSpriteNode(imageName:String, scale:CGFloat, position:CGPoint ) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: imageName)
+        node.setScale(scale)
+        node.position = position
+        addChild(node)
+
+        return node
+    }
 
 
     override func didMove(to view: SKView) {
         //        playerNode =
 
-        guitarNode = SKSpriteNode(imageNamed: "guitar")
-        guitarNode.setScale(0.5)
-        guitarNode.position = CGPoint(x: 100, y: 200)
-        addChild(guitarNode)
+        guitarNode = createSpriteNode(imageName: "guitar", scale: 0.2, position: CGPoint(x: 200, y: 200))
+        pianoSynthNode = createSpriteNode(imageName: "piano", scale: 0.2, position: CGPoint(x: 200, y: 300))
+        //        guitarNode = SKSpriteNode(imageNamed: "guitar")
+        //        guitarNode.setScale(0.2)
+        //        guitarNode.position = CGPoint(x: 100, y: 200)
+        //        addChild(guitarNode)
+        //
+        //        bassNode = SKSpriteNode(imageNamed: "bass")
 
-//        guitarNode
+
+        //        guitarNode
     }
 }
 
